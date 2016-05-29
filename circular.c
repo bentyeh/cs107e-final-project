864b90388d265be8a0c08528536c6433353150b9
Alison Jahansouz <alisonjahansouz@DN0a22823d.SUNet>
2016-05-29 13:24:57 -0700

    setting up handler and circular

--- circular.c
+++ circular.c
@@ -4,7 +4,6 @@
 #include "printf.h"
 
 #define CAPACITY 128
-//static cir_t cir;
 
 
 struct cir {
--- interrupts_handler.c
+++ interrupts_handler.c
@@ -1,1 +1,25 @@
-//stub
+#include "keyboard.h"
+#include "gpio.h"
+#include "gpioevent.h"
+#include "interrupts.h"
+
+
+/* Interrupt Handlers */
+
+
+
+void interrupt_vector(unsigned pc) {
+  if(!gpio_check_event(GPIO_PIN23)){
+  	gprof_vector(pc);
+  }else{
+  	keyboard_vector(pc);
+  }                                                                      
+}
+
+// The rest of these should never be called
+void fast_interrupt_vector(int pc) {}
+void reset_vector(int pc) {}
+void undefined_instruction_vector(int pc) {}
+void software_interrupt_vector(int pc) {}
+void prefetch_abort_vector(int pc) {}
+void data_abort_vector(int pc) {}
