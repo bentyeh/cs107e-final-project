980457171b23c07777e805b552722d21b96f952f
bentyeh <bentyeh@stanford.edu>
2016-06-01 20:21:04 -0700

    Updated drumimage.c, drumimage.h, minimath.h

--- drumimage.h
+++ drumimage.h
@@ -5,4 +5,8 @@ void drumimage_init(unsigned gl_width, unsigned gl_height, unsigned num);
 
 void beat_drum(unsigned drum_num);
 
+void initialize_drums();
+
+void draw_drum(unsigned drum_num, color c);
+
 #endif
