e5f93b32fb5c8a09203c5661a05609589703f022
Benjamin Yeh <bentyeh@stanford.edu>
2016-06-03 11:57:06 -0700

    Updated drumimage_init() to take the graphics buffer size (single or double) as a parameter; also had it clear screen to white. Added test file to test drumimage library

--- drumimage.c
+++ drumimage.c
@@ -12,7 +12,7 @@ static unsigned gl_height;
 const color RESTING_COLOR = GL_BLUE;
 const color BEAT_COLOR = GL_RED;
 
-void drumimage_init(unsigned width, unsigned height, unsigned num) {
+void drumimage_init(unsigned width, unsigned height, unsigned buffer, unsigned num) {
     unsigned i;
 
     gl_width = width;
@@ -21,17 +21,20 @@ void drumimage_init(unsigned width, unsigned height, unsigned num) {
     drum_diameter = min_u(gl_width / (2*num + 1), gl_height);
     drum_array = malloc(num_drums * sizeof(unsigned));
 
-    gl_init(gl_width, gl_height, GL_DOUBLEBUFFER);
+    gl_init(gl_width, gl_height, buffer);
     for(i = 0; i<num_drums; i++) {
         *(drum_array+i) = (2*i+1) * gl_width / (2*num_drums + 1) + drum_diameter/2;
     }
+    gl_clear(GL_WHITE);
     initialize_drums(num_drums);
 }
 
 void beat_drum(unsigned drum_num) {
     draw_drum(drum_num, BEAT_COLOR);
+    gl_swap_buffer();
     delay_us(20000);
     draw_drum(drum_num, RESTING_COLOR);
+    gl_swap_buffer();
 }
 
 void initialize_drums() {
--- drumimage.h
+++ drumimage.h
@@ -1,7 +1,7 @@
 #ifndef DRUMIMAGE_H
 #define DRUMIMAGE_H
 
-void drumimage_init(unsigned gl_width, unsigned gl_height, unsigned num);
+void drumimage_init(unsigned gl_width, unsigned gl_height, unsigned buffer, unsigned num);
 
 void beat_drum(unsigned drum_num);
 
