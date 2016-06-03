3f91f9444bd7855287a0bfe7aa54f97fd34b4a1f
Benjamin Yeh <bentyeh@stanford.edu>
2016-06-03 14:12:32 -0700

    Updated minimath library

--- minimath.h
+++ minimath.h
@@ -1,7 +1,9 @@
 #ifndef MINIMATH_H
 #define MINIMATH_H
 
-double abs (double x);
+// Absolute value
+double abs_d(double x);
+int abs_i(int x);
 
 // Round halfway cases away from zero (ex: round(-0.7) == -1)
 int round(double x);
@@ -13,6 +15,12 @@ int ceil(double x);
 double fpart(double x);
 double rfpart(double x);
 
+// Min, max functions for various data types
+int min_i(int x, int y);
 unsigned min_u(unsigned x, unsigned y);
+double min_d(double x, double y);
+int max_i(int x, int y);
+unsigned max_u(unsigned x, unsigned y);
+double max_d(double x, double y);
 
 #endif
