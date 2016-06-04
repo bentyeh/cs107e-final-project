82b532922dd9e919e6a0afa17d5d8bda16f0982f
Eric Cramer <emcramer@stanford.edu>
2016-06-03 20:53:49 -0700

    added tone.h just in case

--- tone.h
+++ tone.h
@@ -1,8 +1,16 @@
 #ifndef TONE_H
 #define TONE_H
 
+/*
+ * Hardware abstractions making a sound on the Raspberry Pi
+ * audio jack.
+ *
+ * Author: Pat Hanrahan <hanrahan@cs.stanford.edu>
+ * Date: Feb 15, 2016
+ */
+
 #define F_AUDIO 1000000
 
-extern void tone( int freq );
+void tone(int freq);
 
 #endif
